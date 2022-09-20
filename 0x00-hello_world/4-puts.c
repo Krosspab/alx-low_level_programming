8b479049899361e989c12ca75269176519db7e6c
Obeh Maxwell <m.obeh@yahoo.com>
2022-09-20 09:21:37 -0700

    low level program 5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
- *
- * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
+ * main - main block
  * return: 0
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle\n");
 	return(0);
 }
